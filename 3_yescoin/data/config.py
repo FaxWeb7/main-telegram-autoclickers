943ec03445fddaf048226d423c57ef7ace866ff1
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-08-10 11:31:59 +0300

    increased delay between clicks in yescoin

--- 3_yescoin/data/config.py
+++ 3_yescoin/data/config.py
@@ -33,7 +33,7 @@ BOOSTERS = {
 
 DELAYS = {
     'ACCOUNT': global_config.ACC_DELAY,  # delay between connections to accounts (the more accounts, the longer the delay)
-    'CLICKS': [2, 10],   # delay between clicks
+    'CLICKS': [60, 180],   # delay between clicks
     'TASKS': [10, 40]      # delay between completed tasks
 }
 
