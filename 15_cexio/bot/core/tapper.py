08632b5752674d6dc2de529cfce845b65a545ccb
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-09-07 17:28:49 +0300

    fix cexio bot

--- 15_cexio/bot/core/tapper.py
+++ 15_cexio/bot/core/tapper.py
@@ -4,7 +4,6 @@ from time import time
 from urllib.parse import unquote, quote
 
 import aiohttp
-import pytz
 from aiocfscrape import CloudflareScraper
 from aiohttp_proxy import ProxyConnector
 from better_proxy import Proxy
@@ -144,7 +143,7 @@ class Tapper:
                 data_response = json_response['data']
                 self.coin_balance = data_response['balance_USD']
                 logger.info(
-                    f"Account name: {data_response['first_name']} - Balance: <yellow>{data_response['balance_USD']}</yellow> - Btc balance: <yellow>{int(data_response['balance_BTC']) / 100000}</yellow> - Power: <yellow>{data_response['balance_CEXP']}</yellow> CEXP")
+                    f"Account name: {data_response['first_name']} - Balance: <yellow>{data_response['balance_USD']}</yellow> - Btc balance: <yellow>{int(data_response['balance_BTC']) / 100000}</yellow> - Power: <yellow>{data_response['balance']}</yellow> CEXP")
             except Exception as e:
                 logger.error(f"Error while getting user data: {e} .Try again after 30s")
                 await asyncio.sleep(30)
