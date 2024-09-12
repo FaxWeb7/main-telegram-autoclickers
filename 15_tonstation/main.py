386501166121440e0303a7c848838e6f4107165e
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-09-12 21:51:27 +0300

    Revert "remove prev tonstation"
    This reverts commit a017a690669bf815deca6d490d882cfb77c05a01.

--- 15_tonstation/main.py
+++ 15_tonstation/main.py
@@ -1,13 +1,21 @@
+from utils.core.telegram import Accounts
+from utils.starter import start
 import asyncio
-from contextlib import suppress
-
-from bot.utils.launcher import process
 
 
 async def main():
-    await process()
+    action = 1
+    if action == 1:
+        accounts = await Accounts().get_accounts()
+
+        tasks = []
+
+        for thread, account in enumerate(accounts):
+            session_name, phone_number, proxy = account.values()
+            tasks.append(asyncio.create_task(start(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
+
+        await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
-    with suppress(KeyboardInterrupt):
-        asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
