caae30ae5f6e3c63c259bb1b74cd501ba7033bb6
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-09-17 19:08:02 +0300

    fix bugs

--- bots/blum/utils/core/logger.py
+++ bots/blum/utils/core/logger.py
@@ -2,5 +2,5 @@ import sys
 from loguru import logger
 
 logger.remove()
-logger.add(sink=sys.stdout, format="<level>{message}</level>", encoding=sys.stdout.encoding)
+logger.add(sink=sys.stdout, format="<level>{message}</level>")
 logger = logger.opt(colors=True)
