b896c0e545170c81cc8896bc1affccaa483eeda3
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-09-25 17:23:59 +0300

    update blum

--- bots/blum/utils/blum.py
+++ bots/blum/utils/blum.py
@@ -86,7 +86,7 @@ class Blum:
                     logger.info(f"main | Thread {self.thread} | {self.name} | Have {diamonds_balance} diamonds!")
                     games_count = random.randint(*config.MAX_GAMES_COUNT)
                     logger.info(f"main | Thread {self.thread} | {self.name} | Starting play {games_count} games...")
-                    for _ in range(games_count):
+                    for _ in range(min(games_count, diamonds_balance)):
                         await self.game()
                         await asyncio.sleep(random.randint(*config.SLEEP_GAME_TIME))
                         
