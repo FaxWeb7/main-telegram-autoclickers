59971d1ac563e7a806141aeb8c33e2fbaf78eac4
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2024-10-02 22:40:08 +0300

    remove russian language from logging fix encoding errors on windows

--- bots/major/utils/major.py
+++ bots/major/utils/major.py
@@ -15,7 +15,7 @@ import asyncio
 import random
 
 class Major:
-    def __init__(self, thread: int, account: str, proxy : str):
+    def __init__(self, thread: int, account: str, proxy: str):
         self.thread = thread
         self.name = account
         self.ref = config.REF_CODE
@@ -31,16 +31,16 @@ class Major:
             self.client = Client(name=account, api_id=config.API_ID, api_hash=config.API_HASH, workdir=config.WORKDIR, proxy=proxy_client)
         else:
             self.client = Client(name=account, api_id=config.API_ID, api_hash=config.API_HASH, workdir=config.WORKDIR)
-                
+
         if proxy:
             self.proxy = f"{config.PROXY_TYPE}://{proxy.split(':')[2]}:{proxy.split(':')[3]}@{proxy.split(':')[0]}:{proxy.split(':')[1]}"
         else:
             self.proxy = None
-            
+
     async def create_session(self):
         sslcontext = ssl.create_default_context(cafile=certifi.where())
         connector = ProxyConnector().from_url(self.proxy, ssl=sslcontext) if self.proxy else aiohttp.TCPConnector(ssl=sslcontext)
-        
+
         headers = {
             'accept': 'application/json, text/plain, */*',
             'accept-language': 'en-US,en;q=0.9',
@@ -57,7 +57,7 @@ class Major:
         }
 
         return aiohttp.ClientSession(headers=headers, trust_env=True, connector=connector)
-    
+
     async def main(self):
         while True:
             try:
@@ -73,7 +73,7 @@ class Major:
                     logger.error(f"main | Thread {self.thread} | {self.name} | {err}")
                     await self.session.close()
                     return 0
-                
+
                 user = await self.user()
                 await asyncio.sleep(random.uniform(*config.MINI_SLEEP))
                 if random.randint(0,3) == 0 and config.JOIN_SQUAD:
@@ -92,13 +92,13 @@ class Major:
 
                 await self.get_streak()
                 await self.visit()
-                
+
                 await asyncio.sleep(random.uniform(*config.MINI_SLEEP))
                 tasks = await self.get_tasks(is_daily=False)
                 await asyncio.sleep(3*random.uniform(*config.MINI_SLEEP))
                 for task in tasks:
                     if not task['is_completed'] and task['id'] not in config.BLACKLIST_TASKS:
-                        if random.randint(0,3) == 0: 
+                        if random.randint(0,3) == 0:
                             task_info = (await self.do_task(task_id=task['id']))
                             if 'detail' in task_info:
                                 logger.info(f"main | Thread {self.thread} | {self.name} | {task_info['detail']}")
@@ -108,9 +108,9 @@ class Major:
                                 await self.get_tasks(is_daily=False)
                                 await self.get_tasks(is_daily=True)
                                 await asyncio.sleep(random.uniform(*config.TASK_SLEEP))
-                
+
                 await asyncio.sleep(random.uniform(*config.MINI_SLEEP))
-                
+
                 tasks = await self.get_tasks(is_daily=True)
                 for task in tasks:
                     if not task['is_completed'] and task['id'] == 5:
@@ -133,7 +133,7 @@ class Major:
                             logger.info(f"main | Thread {self.thread} | {self.name} | {task_info['detail']}")
                         elif task_info['is_completed']:
                             logger.success(f"main | Thread {self.thread} | {self.name} | CLAIM TASK {task['description']}")
-                        await asyncio.sleep(random.uniform(*config.MINI_SLEEP)) 
+                        await asyncio.sleep(random.uniform(*config.MINI_SLEEP))
                 mini_games = []
                 if config.HOLD_COIN:
                     mini_games.append(self.play_hold_coin)
@@ -145,7 +145,7 @@ class Major:
                 for game in mini_games:
                     await game()
                     await asyncio.sleep(random.uniform(*config.GAME_SLEEP))
-                
+
                 await self.session.close()
                 logger.info(f"main | Thread {self.thread} | {self.name} | All activities in major completed")
                 return 0
@@ -164,19 +164,18 @@ class Major:
             return False
 
         await asyncio.sleep(60)
-        
+
         json_data = {
-            'coins': random.randint(500,1500),  
+            'coins': random.randint(500,1500),
         }
 
         response = await self.session.post('https://major.bot/api/swipe_coin/', json=json_data)
         if 'detail' in await resp.json():
             logger.error(f"swipe_coin | Thread {self.thread} | {self.name} | {await response.json()}")
         elif (await resp.json())['success']:
-            logger.success(f"swipe_coin | Thread {self.thread} | {self.name} | забрал {json_data['coins']} звезд")
+            logger.success(f"swipe_coin | Thread {self.thread} | {self.name} | claimed {json_data['coins']} stars")
         return await response.json()
-    
-    
+
     async def play_roulette(self):
         resp = await self.session.get("https://major.bot/api/roulette/")
         if 'detail' in await resp.json():
@@ -186,7 +185,7 @@ class Major:
         if 'detail' in await resp.json():
             logger.error(f"roulette | Thread {self.thread} | {self.name} | {await response.json()}")
         elif (await resp.json())['success']:
-            logger.success(f"roulette | Thread {self.thread} | {self.name} | забрал {(await response.json())['rating_award']} звезд")
+            logger.success(f"roulette | Thread {self.thread} | {self.name} | claimed {(await response.json())['rating_award']} stars")
         return await response.json()
     
     async def play_hold_coin(self):
@@ -194,20 +193,22 @@ class Major:
         resp = await self.session.get("https://major.bot/api/bonuses/coins/")
         if 'detail' in await resp.json():
             return False
-        
+
         await asyncio.sleep(60)
-        
+
         json_data = {
-            'coins': random.randint(500,850),
+            'coins': random.randint(500, 850),
         }
-        response = await self.session.post('https://major.bot/api/bonuses/coins/', json=json_data)
+        response = await self.session.post("https://major.bot/api/bonuses/coins/", json=json_data)
         if 'detail' in await resp.json():
             logger.error(f"hold_coin | Thread {self.thread} | {self.name} | {await response.json()}")
         elif (await resp.json())['success']:
-            logger.success(f"hold_coin | Thread {self.thread} | {self.name} | забрал {json_data['coins']} звезд")
+            logger.success(
+                f"hold_coin | Thread {self.thread} | {self.name} | claimed {json_data['coins']} stars"
+            )
         return await response.json()
-    
-    async def get_tasks(self, is_daily : bool = False):
+
+    async def get_tasks(self, is_daily: bool = False):
         params = {
             'is_daily': str(is_daily).lower(),
         }
@@ -215,14 +216,13 @@ class Major:
         resp = await self.session.get('https://major.bot/api/tasks/', params=params)
         return await resp.json()
 
-    async def do_task(self, task_id : int):
+    async def do_task(self, task_id: int):
         json_data = {
             'task_id': task_id,
         }
 
         resp = await self.session.post('https://major.bot/api/tasks/', json=json_data)
         return await resp.json()
-    
 
     async def visit(self):
         resp = await self.session.post("https://major.bot/api/user-visits/visit/")
@@ -231,7 +231,7 @@ class Major:
     async def get_streak(self):
         resp = await self.session.get("https://major.bot/api/user-visits/streak/")
         return await resp.json()
-        
+
     async def user(self):
         resp = await self.session.get(f"https://major.bot/api/users/{self.user_info.id}/")
         return await resp.json()
@@ -241,12 +241,12 @@ class Major:
         if 'detail' in await resp.json():
             return False
         if (await resp.json())['status'] == 'ok':
-            logger.success(f"join_squad | Thread {self.thread} | {self.name} | Вступил в сквад")
-            
+            logger.success(f"join_squad | Thread {self.thread} | {self.name} | Joined the squad")
+
     async def login(self):
         try:
             tg_web_data = await self.get_tg_web_data()
-            if tg_web_data == False:
+            if tg_web_data is False:
                 return False
             json_data = {"init_data": tg_web_data}
             resp = await self.session.post("https://major.bot/api/auth/tg/", json=json_data)
@@ -256,7 +256,6 @@ class Major:
         except Exception as err:
             logger.error(f"login | Thread {self.thread} | {self.name} | {err}")
             return False
-
 
     async def get_tg_web_data(self):
         async with self.client:
@@ -270,21 +269,22 @@ class Major:
                             peer=peer,
                             start_param=config.REF_CODE,
                             random_id=random.randint(1, 9999999),
-                        )
-                    )
-                web_view = await self.client.invoke(RequestAppWebView(
-                    peer=await self.client.resolve_peer('major'),
-                    app=InputBotAppShortName(bot_id=await self.client.resolve_peer('major'), short_name="start"),
-                    platform='android',
-                    write_allowed=True,
-                    start_param=self.ref
+                    ))
+                web_view = await self.client.invoke(
+                    RequestAppWebView(
+                        peer=await self.client.resolve_peer('major'),
+                        app=InputBotAppShortName(
+                            bot_id=await self.client.resolve_peer('major'), short_name="start"
+                        ),
+                        platform='android',
+                        write_allowed=True,
+                        start_param=self.ref,
                 ))
 
                 auth_url = web_view.url
-                
+
                 self.user_info = await self.client.get_me()
                 return unquote(string=auth_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0])
             except Exception as err:
                 logger.error(f"get_tg_web_data | Thread {self.thread} | {self.name} | {err}")
                 return False
-  
--- bots/major/utils/telegram.py
+++ bots/major/utils/telegram.py
@@ -15,17 +15,21 @@ class Accounts:
             if file.endswith(".session"):
                 sessions.append(file.replace(".session", ""))
 
-        logger.info(f"Найдено сессий: {len(sessions)}!")
+        logger.info(f"Found sessions: {len(sessions)}!")
         return sessions
 
     async def check_valid_sessions(self, sessions: list):
-        logger.info(f"Проверяю сессии на валидность!")
+        logger.info(f"Checking sessions for validity!")
         valid_sessions = []
         if config.USE_PROXY:
             proxy_dict = {}
-            with open('proxy.txt','r') as file:
-                proxy_list = [i.strip().split() for i in file.readlines() if len(i.strip().split()) == 2]
-                for prox,name in proxy_list:
+            with open("proxy.txt", "r") as file:
+                proxy_list = [
+                    i.strip().split()
+                    for i in file.readlines()
+                    if len(i.strip().split()) == 2
+                ]
+                for prox, name in proxy_list:
                     proxy_dict[name] = prox
             for session in sessions:
                 try:
@@ -33,12 +37,18 @@ class Accounts:
                         proxy = proxy_dict[session]
                         proxy_client = {
                             "scheme": config.PROXY_TYPE,
-                            "hostname": proxy.split(':')[0],
-                            "port": int(proxy.split(':')[1]),
-                            "username": proxy.split(':')[2],
-                            "password": proxy.split(':')[3],
+                            "hostname": proxy.split(":")[0],
+                            "port": int(proxy.split(":")[1]),
+                            "username": proxy.split(":")[2],
+                            "password": proxy.split(":")[3],
                         }
-                        client = Client(name=session, api_id=self.api_id, api_hash=self.api_hash, workdir=self.workdir,proxy=proxy_client)
+                        client = Client(
+                            name=session,
+                            api_id=self.api_id,
+                            api_hash=self.api_hash,
+                            workdir=self.workdir,
+                            proxy=proxy_client,
+                        )
 
                         if await client.connect():
                             valid_sessions.append(session)
@@ -47,21 +57,33 @@ class Accounts:
 
                         await client.disconnect()
                     else:
-                        client = Client(name=session, api_id=self.api_id, api_hash=self.api_hash, workdir=self.workdir)
+                        client = Client(
+                            name=session,
+                            api_id=self.api_id,
+                            api_hash=self.api_hash,
+                            workdir=self.workdir,
+                        )
 
                         if await client.connect():
                             valid_sessions.append(session)
                         else:
                             logger.error(f"{session}.session is invalid")
-                        await client.disconnect()       
+                        await client.disconnect()
                 except:
                     logger.error(f"{session}.session is invalid")
-            logger.success(f"Валидных сессий: {len(valid_sessions)}; Невалидных: {len(sessions)-len(valid_sessions)}")
-                
+            logger.success(
+                f"Valid sessions: {len(valid_sessions)}; Invalid: {len(sessions)-len(valid_sessions)}"
+            )
+
         else:
             for session in sessions:
                 try:
-                    client = Client(name=session, api_id=self.api_id, api_hash=self.api_hash, workdir=self.workdir)
+                    client = Client(
+                        name=session,
+                        api_id=self.api_id,
+                        api_hash=self.api_hash,
+                        workdir=self.workdir,
+                    )
 
                     if await client.connect():
                         valid_sessions.append(session)
@@ -70,7 +92,9 @@ class Accounts:
                     await client.disconnect()
                 except:
                     logger.error(f"{session}.session is invalid")
-            logger.success(f"Валидных сессий: {len(valid_sessions)}; Невалидных: {len(sessions)-len(valid_sessions)}")
+            logger.success(
+                f"Valid sessions: {len(valid_sessions)}; Invalid: {len(sessions)-len(valid_sessions)}"
+            )
         return valid_sessions
 
     async def get_accounts(self):
@@ -78,6 +102,6 @@ class Accounts:
         accounts = await self.check_valid_sessions(sessions)
 
         if not accounts:
-            raise ValueError("Нет валидных сессий")
+            raise ValueError("No valid sessions found")
         else:
             return accounts
